8f6965bded7aba896d0a84815e517c0a97532d65
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2019-10-17 08:36:47 +0300

    INIT: Refactor

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import './index.scss';
 
 import App from './components/_app/App';
-import reducers from './components/_app/reducers';
+import store from './components/_app/store';
 
-
-const composeEnhancers = composeWithDevTools({
-  // Specify here name, actionsBlacklist, actionsCreators and other options
-});
-
-const store = createStore(reducers, composeEnhancers(
-  applyMiddleware(thunk),
-));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
-  , document.getElementById('root'));
+  , document.getElementById('root')
+);
